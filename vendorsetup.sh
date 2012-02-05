1f1414f3a31d2827b5fb093a061aa37487271e16
K900 <k0009000@gmail.com>
2012-02-05 16:49:48 +0400

    Frankenbuild v.1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-#add_lunch_combo full_bravo-userdebug
-#add_lunch_combo full_bravo-eng
-add_lunch_combo ev_bravo-eng
+add_lunch_combo aokp_bravo-userdebug
