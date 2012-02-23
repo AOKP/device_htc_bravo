aeaecfc0c126f1c234572d5976d8ed52c2bc5e0a
K900 <k0009000@gmail.com>
2012-02-23 12:09:47 +0400

    Bootanimation!

--- bravo.mk
+++ bravo.mk
@@ -82,8 +82,8 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
-    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
-
+    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml \
+	vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
 
 # media config xml file
 PRODUCT_COPY_FILES += \
